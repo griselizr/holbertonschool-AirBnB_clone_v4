f3d731465dc3294564c148a4bbca3bc366a00d75
Griseliz <git-commit-address>
2022-12-20 15:24:12 -0400

    update file

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+const $ = window.$;
+$(document).ready(function () {
+    const amenities = {};
+    $(':checkbox').change(function () {
+        if (this.checked) {
+            amenities[$(this).data('id')] = $(this).data('name');
+        }
+        else {
+            delete amenities[$(this).data('id')];
+        }
+        console.log(amenities);
+        $('.amenities h4').text(Object.values(amenities).join(", "));
+
+    });
+});
+
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+    } else {
+        $('div#api_status').removeClass('available');
+    }
+});
